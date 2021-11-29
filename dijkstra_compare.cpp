4113d0ec000431573a3e8726655e030c01bc2f91
Siddharth-Gandhi <ssgandhi602@gmail.com>
2021-11-29 18:22:13 +0530

    Refactored code

--- dijkstra_compare.cpp
+++ dijkstra_compare.cpp
@@ -11,8 +11,9 @@ using namespace std::chrono;
 
 const int COUT_PRECISION = 16;
 const int START_N = 1000;
-const int END_N = 20000;
+const int END_N = 10000;
 const int STEP_N = 1000;
+const vector<int> num_threads = {2, 4, 6, 8, 10, 12};
 
 template <typename T>
 ostream &operator<<(ostream &out, const vector<T> &v) {
@@ -20,45 +21,35 @@ ostream &operator<<(ostream &out, const vector<T> &v) {
     size_t last = v.size() - 1;
     for (size_t i = 0; i < v.size(); ++i) {
         out << v[i];
-        if (i != last)
-            out << ", ";
+        if (i != last) out << ", ";
     }
     out << "}";
     return out;
 }
 
 int main() {
-    // int N = 10000;
-    // cout << "Enter the size of distance matrix (N x N): ";
-    // cin >> N;
-    ofstream serial_csv;
-    ofstream parallel_csv2;
-    ofstream parallel_csv4;
-    ofstream parallel_csv6;
-    ofstream parallel_csv8;
-    ofstream parallel_csv10;
-    ofstream parallel_csv12;
+    // ofstream serial_csv;
+    // ofstream parallel_csv;
+    ofstream dijkstra_time;
+    cout << setprecision(COUT_PRECISION);
 
-    serial_csv.open("csv/serial_time.csv");
-    parallel_csv2.open("csv/parallel_time_2_threads.csv");
-    parallel_csv4.open("csv/parallel_time_4_threads.csv");
-    parallel_csv6.open("csv/parallel_time_6_threads.csv");
-    parallel_csv8.open("csv/parallel_time_8_threads.csv");
-    parallel_csv10.open("csv/parallel_time_10_threads.csv");
-    parallel_csv12.open("csv/parallel_time_12_threads.csv");
+    dijkstra_time.open("dijkstra_time.csv");
 
-    serial_csv << "N,Time(ms)" << endl;
-    parallel_csv2 << "N,Time(ms)" << endl;
-    parallel_csv4 << "N,Time(ms)" << endl;
-    parallel_csv6 << "N,Time(ms)" << endl;
-    parallel_csv8 << "N,Time(ms)" << endl;
-    parallel_csv10 << "N,Time(ms)" << endl;
-    parallel_csv12 << "N,Time(ms)" << endl;
+    // serial_csv.open("dijkstra_csv/serial_time.csv");
+    // serial_csv << "N,Time(ms)";
 
-    // cout << "Time taken by serial Dijkstra's algorithm is: " << endl;
-    // cout << "N \t\t"
-    //      << "Time taken" << endl;
-
+    dijkstra_time << "Number of nodes (N),Serial Time (ms)";
+    for (int num_thread : num_threads) {
+        // dijkstra_time << "," << num_thread << " Threads Time(ms)";
+        dijkstra_time << ","
+                      << "Parallel time with " << num_thread << " threads (ms)";
+    }
+    dijkstra_time << endl;
+    printf(
+        "Starting to calculate Dijkstra's algorithm for number of nodes in the range [%d, %d] "
+        "with an increment of %d\n",
+        START_N, END_N, STEP_N);
+    cout << "Also, testing for the following number of threads: " << num_threads << endl;
     for (int i = START_N; i <= END_N; i += STEP_N) {
         vector<vector<int>> matrix = generate_random_distance_matrix(i);
         // cout << "The randomly generated distance matrix is: \n";
@@ -68,43 +59,23 @@ int main() {
         vector<int> serial_shortest_path = serial_dijkstra(matrix, 0);
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<microseconds>(stop - start);
-        serial_csv << i << "," << duration.count() << endl;
+        // serial_csv << i << "," << duration.count() << endl;
+        dijkstra_time << i << "," << duration.count();
 
-        start = high_resolution_clock::now();
-        vector<int> parallel_dijkstra_path2 = parallel_dijkstra(matrix, 2);
-        stop = high_resolution_clock::now();
-        duration = duration_cast<microseconds>(stop - start);
-        parallel_csv2 << i << "," << duration.count() << endl;
-
-        start = high_resolution_clock::now();
-        vector<int> parallel_dijkstra_path4 = parallel_dijkstra(matrix, 4);
-        stop = high_resolution_clock::now();
-        duration = duration_cast<microseconds>(stop - start);
-        parallel_csv4 << i << "," << duration.count() << endl;
-
-        start = high_resolution_clock::now();
-        vector<int> parallel_dijkstra_path6 = parallel_dijkstra(matrix, 6);
-        stop = high_resolution_clock::now();
-        duration = duration_cast<microseconds>(stop - start);
-        parallel_csv6 << i << "," << duration.count() << endl;
-
-        start = high_resolution_clock::now();
-        vector<int> parallel_dijkstra_path8 = parallel_dijkstra(matrix, 8);
-        stop = high_resolution_clock::now();
-        duration = duration_cast<microseconds>(stop - start);
-        parallel_csv8 << i << "," << duration.count() << endl;
-
-        start = high_resolution_clock::now();
-        vector<int> parallel_dijkstra_path10 = parallel_dijkstra(matrix, 10);
-        stop = high_resolution_clock::now();
-        duration = duration_cast<microseconds>(stop - start);
-        parallel_csv10 << i << "," << duration.count() << endl;
-
-        start = high_resolution_clock::now();
-        vector<int> parallel_dijkstra_path = parallel_dijkstra(matrix, 12);
-        stop = high_resolution_clock::now();
-        duration = duration_cast<microseconds>(stop - start);
-        parallel_csv12 << i << "," << duration.count() << endl;
+        for (int num_thread : num_threads) {
+            // string csv_path =
+            //     "dijkstra_csv/parallel_time_" + to_string(num_thread) + "_threads.csv";
+            // parallel_csv.open(csv_path);
+            // parallel_csv << "N,Time(ms)" << endl;
+            start = high_resolution_clock::now();
+            vector<int> parallel_dijkstra_path = parallel_dijkstra(matrix, num_thread);
+            stop = high_resolution_clock::now();
+            duration = duration_cast<microseconds>(stop - start);
+            // parallel_csv << i << "," << duration.count() << endl;
+            dijkstra_time << "," << duration.count();
+        }
+        dijkstra_time << endl;
     }
+    printf("Finished computations and the results are stored in dijkstra_time.csv");
     return 0;
 }
--- plot_serial_vs_parallel.py
+++ plot_serial_vs_parallel.py
@@ -1,38 +1,16 @@
 import matplotlib.pyplot as plt
-import csv
+import pandas as pd
 
 if __name__ == "__main__":
-    x = []
-    y = []
+    df = pd.read_csv("dijkstra_time.csv")
+    df_columns = df.keys()
 
-    with open('csv/serial_time.csv', 'r') as csvfile:
-        lines = csv.reader(csvfile, delimiter=',')
-        for idx, row in enumerate(lines):
-            if idx == 0:
-                continue
-            x.append(int(row[0]))
-            y.append(int(row[1]))
-    # print(x, y)
-    plt.plot(x, y, color='r', linestyle='dashed',
-             marker='o', label="Time taken by Serial")
-
-    x = []
-    y = []
-
-    with open('csv/parallel_time_6_threads.csv', 'r') as csvfile:
-        lines = csv.reader(csvfile, delimiter=',')
-        for idx, row in enumerate(lines):
-            if idx == 0:
-                continue
-            x.append(int(row[0]))
-            y.append(int(row[1]))
-
-    plt.plot(x, y, color='b', linestyle='dashed',
-             marker='o', label="Time taken by Parallel (6 threads)")
+    df.plot(x=df_columns[0], y=[df_columns[1],
+            df_columns[4]], kind="line", marker="o")
 
     plt.xticks(rotation=25)
-    plt.xlabel('Number of nodes')
-    plt.ylabel('Time Taken')
+    plt.xlabel('Number of nodes (N)')
+    plt.ylabel('Time Taken (ms)')
     plt.title(
         'Dijkstra\'s Algortihm - Serial vs Parallel Execution Time ', fontsize=20)
     plt.grid()
